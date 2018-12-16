61c652e4b5926eb5001be0861697e05dc35a6fd0
scott-hud <philhudson14@hotmail.com>
2018-12-16 17:22:35 +0000

    Cinemachine added etc

---
+++ Game Tool 2 - project 2 week 3/GameTools - Year 2 - Assignment/GameTools_Assignment/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    private GameObject player;
+    private float xRotate, yRotate;
+
+    // Use this for initialization
+    void Start () {
+        player = GameObject.Find("Player");
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        xRotate = Input.GetAxis("Mouse X"); 
+        yRotate += Mathf.Min(50, Mathf.Max(-50, xRotate + Input.GetAxis("Mouse Y")));
+        //player.transform.localRotation = Quaternion.Euler(0, xRotate, 0);
+        player.transform.localRotation = Quaternion.Euler(0, xRotate, 0);
+        transform.localRotation = Quaternion.Euler(yRotate, 0, 0);
+    }
+}
